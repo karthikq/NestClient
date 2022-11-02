0917c64a0b18d6cc1419e71725a194bb180f5fd9
karthikq <kk968346@gmail.com>
2022-11-02 10:57:27 +0530

    uploads ui2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home/Home";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Create from "./pages/Create/Create";
 import Navbar from "./components/Navbar/Navbar";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/create/post" element={<Create />} />
       </Routes>
+      <Toaster />
     </Router>
   );
 }
