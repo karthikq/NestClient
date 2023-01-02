2347a494c6ae7f4e25be9b78de58dc1daa975e74
karthikq <kk968346@gmail.com>
2023-01-02 18:42:09 +0530

    upload

--- src/components/UploadedFiles/Uploadedfiles.jsx
+++ src/components/UploadedFiles/Uploadedfiles.jsx
@@ -1,12 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import "./uploadedfiles.styles.scss";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
-import CustomDialog from "../CustomDialog/CustomDialog"
+import CustomDialog from "../CustomDialog/CustomDialog";
 
-const UploadedFiles = ({ items, handleRemove, activeClose, editState,setUploadedImages }) => {
-  const [openDialogbox,setOpenDialogbox] = useState(false)
+const UploadedFiles = ({
+  items,
+  handleRemove,
+  activeClose,
+  editState,
+  setUploadedImages,
+}) => {
+  const [openDialogbox, setOpenDialogBox] = useState({
+    state: false,
+    type: "",
+  });
+  const [images, setImages] = useState({
+    files: [],
+    urls: [],
+    names: [],
+  });
 
+  const resulttedImages =
+    items.length > 0 && items.length + images.names.length;
+
+  console.log(resulttedImages);
 
   return (
     <div
@@ -18,12 +36,14 @@ const UploadedFiles = ({ items, handleRemove, activeClose, editState,setUploaded
     >
       {editState ? (
         <div className="edit-post-wrapper">
-        { openDialogbox && <CustomDialog
-          setOpen={setOpenDialogbox}
-          open={openDialogbox}
-          setItems={setUploadedImages}
-        />}
-          <div>
+          {openDialogbox.state && (
+            <CustomDialog
+              setOpen={setOpenDialogBox}
+              open={openDialogbox}
+              setItems={setImages}
+            />
+          )}
+          <div className="edit-post-contents">
             {items.length > 0 &&
               items?.map((item, index) => (
                 <div className="uploaded-item ">
@@ -45,10 +65,44 @@ const UploadedFiles = ({ items, handleRemove, activeClose, editState,setUploaded
                   </div>
                 </div>
               ))}
+            {images?.names?.map((item, index) => (
+              <div className="uploaded-item ">
+                <div className="upload-progress-status"></div>
+                <div className="uploaded-checkicon">
+                  <img
+                    src="https://i.ibb.co/9YWkXTT/accept.png"
+                    alt="tickmark"
+                  />
+                </div>
+                <img
+                  className="uploaded-img"
+                  src={images?.urls[index]}
+                  alt="err"
+                />
+                <div className="uploaded-item-details">
+                  <CloseIcon
+                    className="cancel-upload-icon"
+                    onClick={() => handleRemove(index)}
+                  />
+
+                  <p>{item.substring(0, 18)}</p>
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="add-images">
-            <ControlPointIcon className="add-icon"  onClick={()=>setOpenDialogbox(true)}/>
-          </div>
+          {resulttedImages < 3 && (
+            <div className="add-images">
+              <ControlPointIcon
+                className="add-icon"
+                onClick={() =>
+                  setOpenDialogBox({
+                    type: "image",
+                    state: true,
+                  })
+                }
+              />
+            </div>
+          )}
         </div>
       ) : (
         items?.names?.map((item, index) => (
