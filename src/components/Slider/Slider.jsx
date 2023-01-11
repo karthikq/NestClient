b553abdc6729f9c1edded5a2ac0f5a885361f55d
karthikq <kk968346@gmail.com>
2023-01-11 11:48:34 +0530

    lightbox

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,8 +7,14 @@ import { Pagination } from "swiper";
 import "swiper/css/pagination";
 import { EffectCreative } from "swiper";
 import "swiper/css/effect-creative";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+import LaunchIcon from "@mui/icons-material/Launch";
+import { useState } from "react";
 
 const Slider = ({ images }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
   return (
     <>
       <Swiper
@@ -24,16 +30,38 @@ const Slider = ({ images }) => {
             translate: ["100%", 0, 0],
           },
         }}
-        className="myswiper">
+        className="myswiper"
+      >
         {images &&
           images?.map((item) => (
             <SwiperSlide key={item.url}>
               <div className="image-wrapper">
                 <img className="post-image" src={item.url} alt="err" />
+                <LaunchIcon
+                  onClick={() => setIsOpen(true)}
+                  titleAccess="Open Image"
+                  className="lightbox-link-icon"
+                />
               </div>
             </SwiperSlide>
           ))}
       </Swiper>
+      {isOpen && (
+        <Lightbox
+          imageLoadErrorMessage="Loading..."
+          imageTitle={images[photoIndex].name}
+          mainSrc={images[photoIndex].url}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % images.length)
+          }
+        />
+      )}
     </>
   );
 };
